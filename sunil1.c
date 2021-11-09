21568627a8be999028fae69b4caf5c4a94d64b64
panathalasunilkumar <93139903+panathalasunilkumar@users.noreply.github.com>
2021-11-09 12:00:35 +0530

    Add files via upload

---
+++ sunil1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+  void main()
+  {
+  	int a, b,c,d,e,f;
+  	printf(" enter four numbers");
+  	scanf("%d%d%d%d",&a, &b, &c, &d);
+  	e= a+b+c+d;
+  	f=a*b*c*d;
+  	printf("sum of  %d,%d,%d and %d is %d\n",a,b,c,d,e);
+  	printf("Product of %d,%d,%d,and %d is %d",a,b,c,d,f);
+  	
+  	
+  	
+    
+
+
+  	
+  }
